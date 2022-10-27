8c3b714481b9b0242334d2bc44436d6e91ba8a3e
Chenjunqian <chenjunqian0810@foxmail.com>
2022-10-27 18:06:29 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = false
-lvim.colorscheme = "tokyonight"
+lvim.colorscheme = "material"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
@@ -11,14 +11,12 @@ lvim.leader = "space"
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["L"] = "$"
 lvim.keys.normal_mode["H"] = "^"
-
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
-
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
   "bash",
@@ -42,7 +40,10 @@ lvim.builtin.lualine.style = "default"
 lvim.transparent_window = true
 vim.opt.cmdheight = 1
 vim.opt.wrap = true
+vim.g.material_style = "oceanic"
 
 lvim.plugins = {
+  { "lunarvim/colorschemes" },
+  { "marko-cerovac/material.nvim" },
   { "windwp/nvim-ts-autotag" },
 }
