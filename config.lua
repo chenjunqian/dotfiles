bc4716f7b9efdcf1f910db17215a18a4515ada24
Chenjunqian <chenjunqian0810@foxmail.com>
2022-10-27 09:11:56 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,29 +1,25 @@
---[[
-lvim is the global options object
-Linters should be
-filled in as strings with either
-a global executable or a path to
-an executable
-]]
--- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
-
 -- general
 lvim.log.level = "warn"
-lvim.format_on_save = true
-lvim.colorscheme = "material"
+lvim.format_on_save = false
+lvim.colorscheme = "tokyonight"
+-- to disable icons and use a minimalist setup, uncomment the following
+-- lvim.use_icons = false
 
+-- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["L"] = "$"
 lvim.keys.normal_mode["H"] = "^"
+
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
@@ -42,16 +38,13 @@ lvim.builtin.treesitter.ensure_installed = {
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
-lvim.builtin.treesitter.highlight.enabled = true
+lvim.builtin.treesitter.highlight.enable = true
 lvim.builtin.treesitter.autotag.enable = true
 lvim.builtin.lualine.style = "default"
 lvim.transparent_window = true
 vim.opt.cmdheight = 1
 vim.opt.wrap = true
-vim.g.material_style = "oceanic"
 
 lvim.plugins = {
-  { "lunarvim/colorschemes" },
-  { "marko-cerovac/material.nvim" },
   { "windwp/nvim-ts-autotag" },
 }
