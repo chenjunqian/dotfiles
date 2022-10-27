8d2e669f9602c7f65f272f89710713ffe8150a96
Chenjunqian <chenjunqian0810@foxmail.com>
2022-10-27 17:06:02 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -18,8 +18,6 @@ lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
-lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.setup.view.side = "left"
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
