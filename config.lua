36148c32081edd118e3f7472e2523686eaa39909
Chenjunqian <chenjunqian0810@foxmail.com>
2023-01-14 18:10:07 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,7 +2,7 @@
 lvim.log.level = "warn"
 lvim.format_on_save = false
 lvim.colorscheme = "tokyonight"
--- lvim.transparent_window = true
+lvim.transparent_window = true
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
@@ -19,7 +19,7 @@ lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.view.width = 40
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
--- lvim.builtin.nvimtree.setup.actions.open_file.quit_on_open = true
+lvim.builtin.nvimtree.setup.actions.open_file.quit_on_open = true
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
   "bash",
@@ -78,9 +78,53 @@ lvim.plugins = {
       })
       lvim.builtin.which_key.mappings['m'] = {
         name = 'MiniMap',
-        c = {MiniMap.close,"Close MiniMap"},
-        o = {MiniMap.open,"Open MiniMap"},
+        c = { MiniMap.close, "Close MiniMap" },
+        o = { MiniMap.open, "Open MiniMap" },
       }
+    end
+  },
+  {
+    "karb94/neoscroll.nvim",
+    event = "WinScrolled",
+    config = function()
+      require('neoscroll').setup({
+        -- All these keys will be mapped to their corresponding default scrolling animation
+        mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>',
+          '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
+        hide_cursor = true, -- Hide cursor while scrolling
+        stop_eof = true, -- Stop at <EOF> when scrolling downwards
+        use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
+        respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+        cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+        easing_function = nil, -- Default easing function
+        pre_hook = nil, -- Function to run before the scrolling animation starts
+        post_hook = nil, -- Function to run after the scrolling animation ends
+      })
+    end
+  },
+  {
+    "rmagatti/goto-preview",
+    config = function()
+      require('goto-preview').setup {
+        width = 120; -- Width of the floating window
+        height = 25; -- Height of the floating window
+        default_mappings = false; -- Bind default mappings
+        debug = false; -- Print debug information
+        opacity = nil; -- 0-100 opacity level of the floating window where 100 is fully transparent.
+        post_open_hook = nil; -- A function taking two arguments, a buffer and a window to be ran as a hook.
+        -- You can use "default_mappings = true" setup option
+        -- Or explicitly set keybindings
+      }
+
+      vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
+      vim.cmd("nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
+      vim.cmd("nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>")
+      -- lvim.builtin.which_key.mappings['gp'] = {
+      --   name = 'Goto Definition Preview',
+      --   d = { "nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>", "Goto Preview Definition" },
+      --   i = { "nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>",
+      --     "Goto Preiview Implementation" },
+      -- }
     end
   },
 }
