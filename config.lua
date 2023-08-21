ca05fe3ff659093762b9042d38b156cff0a4592b
Chenjunqian <chenjunqian0810@foxmail.com>
2023-08-22 00:59:53 +0800

    Update config.lua
    use dap go adapter support launch.json

--- config.lua
+++ config.lua
@@ -223,25 +223,52 @@ lvim.plugins = {
 
 -- DAP Setting
 local dap = require('dap')
-dap.adapters.delve = {
-    type = 'server',
-    port = '${port}',
-    executable = {
-        command = 'dlv',
-        args = { 'dap', '-l', '127.0.0.1:${port}' },
+require('dap.ext.vscode').load_launchjs()
+
+dap.adapters.go = function(callback, config)
+    local stdout = vim.loop.new_pipe(false)
+    local handle
+    local pid_or_err
+    local host = config.host or "127.0.0.1"
+    local port = config.port or "38697"
+    local addr = string.format("%s:%s", host, port)
+    local opts = {
+        stdio = { nil, stdout },
+        args = { "dap", "-l", addr },
+        detached = true,
     }
-}
+    handle, pid_or_err = vim.loop.spawn("dlv", opts, function(code)
+        stdout:close()
+        handle:close()
+        if code ~= 0 then
+            print("dlv exited with code", code)
+        end
+    end)
+    assert(handle, "Error running dlv: " .. tostring(pid_or_err))
+    stdout:read_start(function(err, chunk)
+        assert(not err, err)
+        if chunk then
+            vim.schedule(function()
+                require("dap.repl").append(chunk)
+            end)
+        end
+    end)
+    -- Wait for delve to start
+    vim.defer_fn(function()
+        callback({ type = "server", host = "127.0.0.1", port = port })
+    end, 100)
+end
 
 -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
 dap.configurations.go = {
     {
-        type = "delve",
+        type = "go",
         name = "Debug",
         request = "launch",
         program = "${file}"
     },
     {
-        type = "delve",
+        type = "go",
         name = "Debug test", -- configuration for debugging test files
         request = "launch",
         mode = "test",
@@ -249,7 +276,7 @@ dap.configurations.go = {
     },
     -- works with go.mod packages and sub packages
     {
-        type = "delve",
+        type = "go",
         name = "Debug test (go.mod)",
         request = "launch",
         mode = "test",
