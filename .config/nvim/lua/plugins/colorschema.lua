ee8cf2ce4b988a4eab5762e44f00fa7ca6839684
Chenjunqian <chenjunqian0810@foxmail.com>
2025-12-30 00:15:08 +0800

    Add colorschema configuration for tokyonight.nvim

---
+++ .config/nvim/lua/plugins/colorschema.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent", -- make the sidebars (like neo-tree) transparent
+        floats = "transparent", -- make floating windows transparent
+      },
+    },
+  },
+}
