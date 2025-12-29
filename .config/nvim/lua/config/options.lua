f54e51ffc3d01a504297ff091856b3012759fb5c
Chenjunqian <chenjunqian0810@foxmail.com>
2025-12-30 00:14:12 +0800

    Update Neovim options for formatting and line handling

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,3 +3,9 @@
 -- Add any additional options here
 
 vim.opt.relativenumber = false
+vim.g.autoformat = false
+vim.opt.autoread = true
+vim.opt.wrap = true          -- Enable soft wrap
+vim.opt.linebreak = true     -- Don't break words in the middle
+vim.opt.breakindent = true   -- Wrapped lines retain indentation
+vim.g.transparent_enabled = true
