47be144e0e14eec17eb1996693c2097750457cb1
Chenjunqian <chenjunqian0810@foxmail.com>
2025-12-30 00:13:49 +0800

    Add new keymaps for terminal and buffer navigation

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,8 +4,10 @@
 
 -- Buffer navigation
 vim.keymap.set("n", "<Tab>", ":bnext<CR>", { desc = "Next buffer" })
-vim.keymap.set("n", "<C-Tab>", ":bprevious<CR>", { desc = "Previous buffer" })
+vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", { desc = "Previous buffer" })
 
--- Line navigation
+vim.keymap.set("n", "<C-\\>", function() Snacks.terminal()
+end, { desc = "Terminal (Root Dir)" })
+vim.keymap.set("t", "<C-\\>", "<cmd>close<cr>", { desc = "Hide Terminal" })
 vim.keymap.set("n", "L", "$", { desc = "Go to end of line" })
 vim.keymap.set("n", "H", "^", { desc = "Go to beginning of line" })
