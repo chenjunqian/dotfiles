37816aaf858361a983b573806f4e84ba99a6ed1b
Chenjunqian <chenjunqian0810@foxmail.com>
2025-12-30 20:34:07 +0800

    Add neo-tree plugin configuration

---
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-neo-tree/nvim-neo-tree",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+    },
+  },
+}
